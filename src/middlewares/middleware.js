9033da95c0dbf3f4080bf57f8b438e93514219d6
jcgsr <cabralguerra@gmail.com>
2020-05-02 10:19:19 -0300

    cadastrar, login, logout

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,12 +1,15 @@
 exports.middlewareGlobal = (req, res, next) => {
-    res.locals.umaVariavelLocal = 'Este é o valor da variável local'
+    res.locals.errors = req.flash('errors')
+    res.locals.success = req.flash('success')
+    res.locals.user = req.session.user
     next()
 }
 
 exports.checkCSRFError = (err, req, res, next) => {
-    if(err && 'EBADCSRFTOKEN' === err.code){
+    if(err){
         return res.render('404')
     }
+    next()
 }
 
 exports.csrfMiddleware = (req, res, next) => {
