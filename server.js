ae7e56f3a1518333a8442e166383da5ce9067209
jcgsr <cabralguerra@gmail.com>
2020-05-05 09:16:05 -0300

    heroku port

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(csrfMiddleware)
 app.use(routes)
 
 app.on('Pronto', () => {
-    app.listen(3000, () => {
+    app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor executando na porta 3000');
     })
 })
